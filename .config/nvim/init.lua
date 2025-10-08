7df802dce4efb532806d230e5441378ff97c1848
Viggo Kirkegaard Helstrup <vkirkegaardhelstrup@gmail.com>
2025-10-08 18:29:28 +0200

    astrovim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,25 +1,27 @@
-local base_dir = vim.env.LUNARVIM_BASE_DIR
-  or (function()
-    local init_path = debug.getinfo(1, "S").source
-    return init_path:sub(2):match("(.*[/\\])"):sub(1, -2)
-  end)()
+-- This file simply bootstraps the installation of Lazy.nvim and then calls other files for execution
+-- This file doesn't necessarily need to be touched, BE CAUTIOUS editing this file and proceed at your own risk.
+local lazypath = vim.env.LAZY or vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 
-if not vim.tbl_contains(vim.opt.rtp:get(), base_dir) then
-  vim.opt.rtp:prepend(base_dir)
+if not (vim.env.LAZY or (vim.uv or vim.loop).fs_stat(lazypath)) then
+  -- stylua: ignore
+  local result = vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
+  if vim.v.shell_error ~= 0 then
+    -- stylua: ignore
+    vim.api.nvim_echo({ { ("Error cloning lazy.nvim:\n%s\n"):format(result), "ErrorMsg" }, { "Press any key to exit...", "MoreMsg" } }, true, {})
+    vim.fn.getchar()
+    vim.cmd.quit()
+  end
 end
 
-require("lvim.bootstrap"):init(base_dir)
+vim.opt.rtp:prepend(lazypath)
 
-require("lvim.config"):load()
+-- validate that lazy is available
+if not pcall(require, "lazy") then
+  -- stylua: ignore
+  vim.api.nvim_echo({ { ("Unable to load lazy from: %s\n"):format(lazypath), "ErrorMsg" }, { "Press any key to exit...", "MoreMsg" } }, true, {})
+  vim.fn.getchar()
+  vim.cmd.quit()
+end
 
-local plugins = require "lvim.plugins"
-
-require("lvim.plugin-loader").load { plugins, lvim.plugins }
-
-require("lvim.core.theme").setup()
-
-local Log = require "lvim.core.log"
-Log:debug "Starting LunarVim"
-
-local commands = require "lvim.core.commands"
-commands.load(commands.defaults)
+require "lazy_setup"
+require "polish"
