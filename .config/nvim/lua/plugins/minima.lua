fa0b17d5d2c9d4f8c4025e62cd90bccdae7c602f
Viggo Kirkegaard Helstrup <vkirkegaardhelstrup@gmail.com>
2025-11-05 18:12:04 +0100

    user.lua to minima.lua

--- .config/nvim/lua/plugins/minima.lua
+++ .config/nvim/lua/plugins/minima.lua
@@ -1,6 +1,12 @@
 return {
   {
     vim.keymap.set("n", "-m", "<cmd>!make<cr>", {desc = "Run Make"}),
+    
+    vim.keymap.set("n", "-p", "", {desc = "PlatformIO"}),
+    vim.keymap.set("n", "-pd", "<cmd>!pio run -t cmpiledb<CR>", {desc = "Make pio compiledb"}),
+    vim.keymap.set("n", "-pu", "<cmd>!pio run -t upload<CR>", {desc = "Upload pio project"}),
+    vim.keymap.set("n", "-pm", "<cmd>!pio run<CR>", {desc = "Make pio project"}),
+    vim.keymap.set("n", "-pc", "<cmd>!pio run -t clean<CR>", {desc = "Clean pio project"}),
 
     vim.api.nvim_create_autocmd("FileType", {
       pattern = "tex",
