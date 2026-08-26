cd53c7e578055575a22e6cc48ac14f7a5457ca35
Poellebob <91906512+Poellebob@users.noreply.github.com>
2026-08-26 09:56:15 +0200

    Refactor LSP configuration for clangd in cpp.nix

--- nixvim/languages/cpp.nix
+++ nixvim/languages/cpp.nix
@@ -6,23 +6,19 @@
 }:
 
 {
-  plugins.lsp = {
-    enable = true;
+  config = lib.mkIf config.minima.vim.enable {
+    programs.nixvim.lsp.servers.clangd = {
+      enable = true;
 
-    servers = {
-      clangd = {
-        enable = true;
-
-        settings = {
-          clangd = {
-            cmd = [
-              "clangd"
-              "--background-index"
-              "--clang-tidy"
-              "--header-insertion=iwyu"
-              "--completion-style=detailed"
-            ];
-          };
+      settings = {
+        clangd = {
+          cmd = [
+            "clangd"
+            "--background-index"
+            "--clang-tidy"
+            "--header-insertion=iwyu"
+            "--completion-style=detailed"
+          ];
         };
       };
     };
