1b7ac1c24731a4b3504e2e3ee2c5a65f84afb3bb
Poellebob <91906512+Poellebob@users.noreply.github.com>
2026-08-26 14:33:27 +0200

    Add ccls support for PlatformIO projects

--- nixvim/languages/cpp.nix
+++ nixvim/languages/cpp.nix
@@ -5,18 +5,48 @@
   ...
 }:
 
+with lib;
 {
   config = lib.mkIf config.minima.vim.enable {
-    programs.nixvim.lsp.servers.clangd = {
-      enable = true;
-      config = {
+    programs.nixvim.lsp.servers = {
+      # Used for PlatformIO projects
+      ccls = {
+        enable = true;
+        package = pkgs.ccls;
+        cmd = [ "ccls" ];
+        filetypes = [ "c" "cpp" ];
+        root_dir = mkRaw ''
+          function(fname, bufnr)
+            local util = require("lspconfig.util")
+            return util.root_pattern("platformio.ini")(fname)
+          end
+        '';
+      };
+
+      # Used for everything else
+      clangd = {
+        enable = true;
         cmd = [
           "clangd"
           "--background-index"
           "--clang-tidy"
         ];
         filetypes = [ "c" "cpp" ];
-        root_markers = [ "compile_commands.json" "compile_flags.txt" ".clangd" ".git" ];
+        root_dir = mkRaw ''
+          function(fname, bufnr)
+            local util = require("lspconfig.util")
+            -- bail out if this is a PlatformIO project; let ccls take it
+            if util.root_pattern("platformio.ini")(fname) then
+              return nil
+            end
+            return util.root_pattern(
+              "compile_commands.json",
+              "compile_flags.txt",
+              ".clangd",
+              ".git"
+            )(fname)
+          end
+        '';
       };
     };
   };
