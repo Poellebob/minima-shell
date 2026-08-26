f475c4b2ba71857395b904014cc75366795bb5e5
Poellebob <91906512+Poellebob@users.noreply.github.com>
2026-08-26 16:46:44 +0200

    Implement LSP stop on platformio.ini detection
    Add autocommand to stop LSP if platformio.ini is found

--- nixvim/autocommands.nix
+++ nixvim/autocommands.nix
@@ -8,6 +8,24 @@
         pattern = [ "markdown" ];
         command = "setlocal spell spelllang=en_us";
       }
+      {
+        event = [ "VimEnter" "DirChanged" ];
+        callback = {
+          __raw = ''
+            function()
+              -- look for platformio.ini in cwd (project root)
+              local root = vim.fn.getcwd()
+              local pio_ini = root .. "/platformio.ini"
+  
+              if vim.fn.filereadable(pio_ini) == 1 then
+                vim.schedule(function()
+                  vim.cmd("LspStop")
+                end)
+              end
+            end
+          '';
+        };
+      }
     ] ++ map (a: {
       event = a.event;
       pattern = a.pattern;
