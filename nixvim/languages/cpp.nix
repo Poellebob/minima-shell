dc1d083037007af22ff474eff1c35aecb2ef3791
Poellebob <91906512+Poellebob@users.noreply.github.com>
2026-08-26 09:51:29 +0200

    Refactor C++ plugin configuration for nixvim

--- nixvim/languages/cpp.nix
+++ nixvim/languages/cpp.nix
@@ -6,17 +6,25 @@
 }:
 
 {
-  config = lib.mkIf config.minima.vim.enable {
-    programs.nixvim.plugins = {
-      lsp.servers.clangd.enable = true;
+  plugins.lsp = {
+    enable = true;
 
-      lint.linters.cppcheck.cmd = "${pkgs.cppcheck}/bin/cppcheck";
-      lint.lintersByFt.c = [ "cppcheck" ];
-      lint.lintersByFt.cpp = [ "cppcheck" ];
+    servers = {
+      clangd = {
+        enable = true;
 
-      conform-nvim.settings.formatters_by_ft.c = [ "clang_format" ];
-      conform-nvim.settings.formatters_by_ft.cpp = [ "clang_format" ];
-      conform-nvim.settings.formatters.clang_format.command = "${pkgs.clang-tools}/bin/clang-format";
+        settings = {
+          clangd = {
+            cmd = [
+              "clangd"
+              "--background-index"
+              "--clang-tidy"
+              "--header-insertion=iwyu"
+              "--completion-style=detailed"
+            ];
+          };
+        };
+      };
     };
   };
 }
