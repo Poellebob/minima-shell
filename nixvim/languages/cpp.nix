0bf234baa4fb58d79a26fec82b619e95053115ac
Poellebob <91906512+Poellebob@users.noreply.github.com>
2026-08-26 14:42:00 +0200

    Remove clangd and PlatformIO support from cpp.nix
    Removed clangd configuration and PlatformIO project handling from cpp.nix.

--- nixvim/languages/cpp.nix
+++ nixvim/languages/cpp.nix
@@ -9,7 +9,6 @@ with lib;
 {
   config = lib.mkIf config.minima.vim.enable {
     programs.nixvim.lsp.servers = {
-      # Used for PlatformIO projects
       ccls = {
         enable = true;
         package = pkgs.ccls;
@@ -29,39 +28,6 @@ with lib;
             ".git"
           ];
         };
-
-        root_dir = mkRaw ''
-          function(fname, bufnr)
-            local util = require("lspconfig.util")
-            return util.root_pattern("platformio.ini")(fname)
-          end
-        '';
-      };
-
-      # Used for everything else
-      clangd = {
-        enable = true;
-        cmd = [
-          "clangd"
-          "--background-index"
-          "--clang-tidy"
-        ];
-        filetypes = [ "c" "cpp" ];
-        root_dir = mkRaw ''
-          function(fname, bufnr)
-            local util = require("lspconfig.util")
-            -- bail out if this is a PlatformIO project; let ccls take it
-            if util.root_pattern("platformio.ini")(fname) then
-              return nil
-            end
-            return util.root_pattern(
-              "compile_commands.json",
-              "compile_flags.txt",
-              ".clangd",
-              ".git"
-            )(fname)
-          end
-        '';
       };
     };
   };
