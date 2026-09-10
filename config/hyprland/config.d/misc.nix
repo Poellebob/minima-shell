8dba14a86b216b0c96c00ba209ea7fcb8699fddd
Viggo Kirkegaard Helstrup <vkirkegaardhelstrup@gmail.com>
2026-09-10 18:52:30 +0200

    discard initial workspace on windows

--- config/hyprland/config.d/misc.nix
+++ config/hyprland/config.d/misc.nix
@@ -4,6 +4,7 @@
       disable_hyprland_logo = true,
       disable_splash_rendering = true,
       disable_xdg_env_checks = true,
+      initial_workspace_tracking = false,
       key_press_enables_dpms = true,
       session_lock_xray = true,
     },
